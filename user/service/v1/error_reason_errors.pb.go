060bf7b13ca9d02c6efeec34aa94787e5a994937
waynewu411 <waynewu411@gmail.com>
2025-04-22 14:39:51 +0800

    Add OPERATOR_ID_NOT_FOUND_IN_CONTEXT error to user

--- user/service/v1/error_reason_errors.pb.go
+++ user/service/v1/error_reason_errors.pb.go
@@ -274,3 +274,15 @@ func IsOauthProviderNotSupported(err error) bool {
 func ErrorOauthProviderNotSupported(format string, args ...interface{}) *errors.Error {
 	return errors.New(500, ErrorReason_OAUTH_PROVIDER_NOT_SUPPORTED.String(), fmt.Sprintf(format, args...))
 }
+
+func IsOperatorIdNotFoundInContext(err error) bool {
+	if err == nil {
+		return false
+	}
+	e := errors.FromError(err)
+	return e.Reason == ErrorReason_OPERATOR_ID_NOT_FOUND_IN_CONTEXT.String() && e.Code == 500
+}
+
+func ErrorOperatorIdNotFoundInContext(format string, args ...interface{}) *errors.Error {
+	return errors.New(500, ErrorReason_OPERATOR_ID_NOT_FOUND_IN_CONTEXT.String(), fmt.Sprintf(format, args...))
+}
