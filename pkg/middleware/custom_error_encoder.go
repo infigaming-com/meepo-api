7303579e88cad2949ac2ff8143840762d169e4b1
waynewu411 <waynewu411@gmail.com>
2025-05-29 13:24:39 +1000

    Update CustomErrorEncoder

--- pkg/middleware/custom_error_encoder.go
+++ pkg/middleware/custom_error_encoder.go
@@ -5,28 +5,31 @@ import (
 	"net/http"
 
 	"github.com/go-kratos/kratos/v2/errors"
+	khttp "github.com/go-kratos/kratos/v2/transport/http"
 )
 
-func CustomErrorEncoder(w http.ResponseWriter, r *http.Request, err error, errorReasonMap map[string]int32) {
-	if err == nil {
-		return
+func CustomErrorEncoder(errorReasonMap map[string]int32) khttp.EncodeErrorFunc {
+	return func(w http.ResponseWriter, r *http.Request, err error) {
+		if err == nil {
+			return
+		}
+
+		se := errors.FromError(err)
+
+		code, ok := errorReasonMap[se.Reason]
+		if !ok {
+			code = 500
+		}
+
+		resp := map[string]any{
+			"code":    code,
+			"message": se.Message,
+			"reason":  se.Reason,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		json.NewEncoder(w).Encode(resp)
 	}
-
-	se := errors.FromError(err)
-
-	code, ok := errorReasonMap[se.Reason]
-	if !ok {
-		code = 500
-	}
-
-	resp := map[string]any{
-		"code":    code,
-		"message": se.Message,
-		"reason":  se.Reason,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-
-	json.NewEncoder(w).Encode(resp)
 }
