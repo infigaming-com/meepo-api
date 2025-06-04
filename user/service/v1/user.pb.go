dea21ea3c6011d44f449b03552c059e538c5efe5
Jerry Xu <zhaoyunxu.job@gmail.com>
2025-06-04 17:12:07 +1000

    Remove operator_id

--- user/service/v1/user.pb.go
+++ user/service/v1/user.pb.go
@@ -1572,11 +1572,9 @@ func (x *GetOperatorTagsConfigResponse) GetFollowParent() bool {
 	return false
 }
 
-// GetOperatorTagsRequest contains the operator ID to retrieve tags for.
+// GetOperatorTagsRequest does not contain any parameters. It will return all tags of user's operator.
 type GetOperatorTagsRequest struct {
-	state protoimpl.MessageState `protogen:"open.v1"`
-	// The ID of the operator to get tags for.
-	OperatorId    int64 `protobuf:"varint,1,opt,name=operator_id,json=operatorId,proto3" json:"operator_id,omitempty"`
+	state         protoimpl.MessageState `protogen:"open.v1"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -1609,13 +1607,6 @@ func (x *GetOperatorTagsRequest) ProtoReflect() protoreflect.Message {
 // Deprecated: Use GetOperatorTagsRequest.ProtoReflect.Descriptor instead.
 func (*GetOperatorTagsRequest) Descriptor() ([]byte, []int) {
 	return file_user_service_v1_user_proto_rawDescGZIP(), []int{24}
-}
-
-func (x *GetOperatorTagsRequest) GetOperatorId() int64 {
-	if x != nil {
-		return x.OperatorId
-	}
-	return 0
 }
 
 // GetOperatorTagsResponse contains the list of tags associated with the operator or parent operator if follow_parent is true.
@@ -4929,10 +4920,8 @@ const file_user_service_v1_user_proto_rawDesc = "" +
 	"\voperator_id\x18\x01 \x01(\x03R\n" +
 	"operatorId\"D\n" +
 	"\x1dGetOperatorTagsConfigResponse\x12#\n" +
-	"\rfollow_parent\x18\x01 \x01(\bR\ffollowParent\"9\n" +
-	"\x16GetOperatorTagsRequest\x12\x1f\n" +
-	"\voperator_id\x18\x01 \x01(\x03R\n" +
-	"operatorId\"-\n" +
+	"\rfollow_parent\x18\x01 \x01(\bR\ffollowParent\"\x18\n" +
+	"\x16GetOperatorTagsRequest\"-\n" +
 	"\x17GetOperatorTagsResponse\x12\x12\n" +
 	"\x04tags\x18\x01 \x03(\tR\x04tags\"\x14\n" +
 	"\x12GetUserTagsRequest\")\n" +
--- user/service/v1/user.pb.validate.go
+++ user/service/v1/user.pb.validate.go
@@ -2667,8 +2667,6 @@ func (m *GetOperatorTagsRequest) validate(all bool) error {
 
 	var errors []error
 
-	// no validation rules for OperatorId
-
 	if len(errors) > 0 {
 		return GetOperatorTagsRequestMultiError(errors)
 	}
