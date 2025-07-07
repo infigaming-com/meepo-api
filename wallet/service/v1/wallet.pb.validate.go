50c3d5e24d49a32882a4f3c22c1f55cb6b38ea48
Jerry Xu <zhaoyunxu.job@gmail.com>
2025-07-07 22:24:20 +1000

    Add ListOperatorBalancesRequest

--- wallet/service/v1/wallet.pb.validate.go
+++ wallet/service/v1/wallet.pb.validate.go
@@ -6598,6 +6598,39 @@ func (m *ListOperatorBalancesRequest) validate(all bool) error {
 
 	var errors []error
 
+	// no validation rules for RealOperatorId
+
+	// no validation rules for OperatorType
+
+	if all {
+		switch v := interface{}(m.GetOperatorContext()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ListOperatorBalancesRequestValidationError{
+					field:  "OperatorContext",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ListOperatorBalancesRequestValidationError{
+					field:  "OperatorContext",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetOperatorContext()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ListOperatorBalancesRequestValidationError{
+				field:  "OperatorContext",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if m.Page != nil {
 		// no validation rules for Page
 	}
