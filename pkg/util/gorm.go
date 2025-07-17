d7af9500a3a2e8ca65f5d1afc999790e00ba186f
waynewu411 <waynewu411@gmail.com>
2025-07-17 19:49:30 +1000

    Update BuildOperatorContextFiltersQuery() for OperatorContextFilters

--- pkg/util/gorm.go
+++ pkg/util/gorm.go
@@ -25,22 +25,25 @@ func BuildOperatorContextQuery(query *gorm.DB, operatorContext *common.OperatorC
 	return query
 }
 
-func BuildOperatorContextsFilterQuery(query *gorm.DB, RetailerOperatorContexts []*common.OperatorContext, CompanyOperatorContexts []*common.OperatorContext, OperatorContexts []*common.OperatorContext) *gorm.DB {
-	if len(OperatorContexts) > 0 {
+func BuildOperatorContextFiltersQuery(query *gorm.DB, operatorContextFilters *common.OperatorContextFilters) *gorm.DB {
+	if operatorContextFilters == nil {
+		return query
+	}
+	if len(operatorContextFilters.OperatorContexts) > 0 {
 		// build a list of operatorIdsList
-		operatorIdsList := lo.Map(OperatorContexts, func(context *common.OperatorContext, _ int) []int64 {
+		operatorIdsList := lo.Map(operatorContextFilters.OperatorContexts, func(context *common.OperatorContext, _ int) []int64 {
 			return []int64{context.OperatorId, context.CompanyOperatorId, context.RetailerOperatorId, context.SystemOperatorId}
 		})
 		query = query.Where("(operator_id, company_operator_id, retailer_operator_id, system_operator_id) IN ?", operatorIdsList)
-	} else if len(CompanyOperatorContexts) > 0 {
+	} else if len(operatorContextFilters.CompanyOperatorContexts) > 0 {
 		// build a list of operatorIdsList
-		operatorIdsList := lo.Map(CompanyOperatorContexts, func(context *common.OperatorContext, _ int) []int64 {
+		operatorIdsList := lo.Map(operatorContextFilters.CompanyOperatorContexts, func(context *common.OperatorContext, _ int) []int64 {
 			return []int64{context.CompanyOperatorId, context.RetailerOperatorId, context.SystemOperatorId}
 		})
 		query = query.Where("(company_operator_id, retailer_operator_id, system_operator_id) IN ?", operatorIdsList)
-	} else if len(RetailerOperatorContexts) > 0 {
+	} else if len(operatorContextFilters.RetailerOperatorContexts) > 0 {
 		// build a list of operatorIdsList
-		operatorIdsList := lo.Map(RetailerOperatorContexts, func(context *common.OperatorContext, _ int) []int64 {
+		operatorIdsList := lo.Map(operatorContextFilters.RetailerOperatorContexts, func(context *common.OperatorContext, _ int) []int64 {
 			return []int64{context.RetailerOperatorId, context.SystemOperatorId}
 		})
 		query = query.Where("(retailer_operator_id, system_operator_id) IN ?", operatorIdsList)
