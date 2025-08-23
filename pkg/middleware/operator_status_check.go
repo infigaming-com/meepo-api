386c2a5a96fe12d94bd7c3d40a63900a349a09d4
isphinx <isphinx@me.com>
2025-08-23 12:12:15 +0800

    use kratos erros

--- pkg/middleware/operator_status_check.go
+++ pkg/middleware/operator_status_check.go
@@ -2,10 +2,9 @@ package middleware
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"slices"
 
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/infigaming-com/meepo-api/pkg/util"
 
@@ -17,15 +16,15 @@ func OperatorStatusCheck() middleware.Middleware {
 		return func(ctx context.Context, req any) (reply any, err error) {
 			requestInfo, ok := mctx.GetRequestInfo(ctx)
 			if !ok {
-				return nil, errors.New("GetRequestInfo failed")
+				return nil, errors.New(403, "FORBIDDEN", "invalid system operatorId")
 			}
 			operatorInfo, ok := mctx.GetOperatorInfo(ctx)
 			if !ok {
-				return nil, errors.New("GetOperatorInfo failed")
+				return nil, errors.New(403, "FORBIDDEN", "GetOperatorInfo failed")
 			}
 
 			if operatorInfo.Status == util.OperatorStatusPending && slices.Contains(operatorInfo.StatusLaunchWhitelist, requestInfo.ClientIP) {
-				return nil, fmt.Errorf("IP is not allow to access (%s)", requestInfo.ClientIP)
+				return nil, errors.New(403, "FORBIDDEN", "IP is not allow to access")
 			}
 			return handler(ctx, req)
 		}
