bff17138ac59b1c4f95e0a1e3ff494837b13ab90
isphinx <isphinx@me.com>
2025-09-20 12:23:53 +0800

    register id number only

--- user/service/v1/user.pb.validate.go
+++ user/service/v1/user.pb.validate.go
@@ -300,37 +300,8 @@ func (m *RegisterRequest) validate(all bool) error {
 
 	}
 
-	if m.UserIdentity != nil {
-
-		if all {
-			switch v := interface{}(m.GetUserIdentity()).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, RegisterRequestValidationError{
-						field:  "UserIdentity",
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, RegisterRequestValidationError{
-						field:  "UserIdentity",
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(m.GetUserIdentity()).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return RegisterRequestValidationError{
-					field:  "UserIdentity",
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
+	if m.IdNumber != nil {
+		// no validation rules for IdNumber
 	}
 
 	if len(errors) > 0 {
