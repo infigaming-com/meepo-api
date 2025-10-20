f4789aedf87edc8ebd36f6dfa96f9029ebfdb6ec
Jerry Xu <zhaoyunxu.job@gmail.com>
2025-10-20 23:55:23 +1100

    Refactor Campaign parameters

--- pkg/util/affiliate.go
+++
@@ -1,9 +0,0 @@
-package util
-
-const (
-	CommissionPlanTypeCPA      = "cpa"
-	CommissionPlanTypeRevShare = "rev_share"
-	CommissionPlanTypeCPL      = "cpl"
-	CommissionPlanTypeCPC      = "cpc"
-	CommissionPlanTypeFlatFee  = "flat_fee"
-)
