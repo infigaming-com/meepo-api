aefef6a431d4922b36179bd73d74a97bb3f4fd66
davy <davy@speedix.com>
2025-11-28 15:46:15 +0800

    Add AffectedCredit struct and update GameTransactionEvent to include affected credits

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -112,43 +112,65 @@ type EventInfo struct {
 	Status                    string `json:"status,omitempty"`
 }
 
+type AffectedCredit struct {
+	Amount                                string `json:"amount,omitempty"`
+	CreditId                              int64  `json:"credit_id,omitempty"`
+	AmountUsd                             string `json:"amount_usd,omitempty"`
+	CashAmount                            string `json:"cash_amount,omitempty"`
+	CashAmountUsd                         string `json:"cash_amount_usd,omitempty"`
+	SettlementCurrency                    string `json:"settlement_currency,omitempty"`
+	OperatorBonusAmount                   string `json:"operator_bonus_amount,omitempty"`
+	ProviderBonusAmount                   string `json:"provider_bonus_amount,omitempty"`
+	AmountReportingCurrency               string `json:"amount_reporting_currency,omitempty"`
+	OperatorBonusAmountUsd                string `json:"operator_bonus_amount_usd,omitempty"`
+	ProviderBonusAmountUsd                string `json:"provider_bonus_amount_usd,omitempty"`
+	AmountSettlementCurrency              string `json:"amount_settlement_currency,omitempty"`
+	CashAmountReportingCurrency           string `json:"cash_amount_reporting_currency,omitempty"`
+	CashAmountSettlementCurrency          string `json:"cash_amount_settlement_currency,omitempty"`
+	OperatorBonusAmountReportingCurrency  string `json:"operator_bonus_amount_reporting_currency,omitempty"`
+	ProviderBonusAmountReportingCurrency  string `json:"provider_bonus_amount_reporting_currency,omitempty"`
+	OperatorBonusAmountSettlementCurrency string `json:"operator_bonus_amount_settlement_currency,omitempty"`
+	ProviderBonusAmountSettlementCurrency string `json:"provider_bonus_amount_settlement_currency,omitempty"`
+}
+
 type GameTransactionEvent struct {
-	OperatorId                   int64       `json:"operator_id,omitempty"`
-	OperatorName                 string      `json:"operator_name,omitempty"`
-	ProviderId                   string      `json:"provider_id,omitempty"`
-	ProviderName                 string      `json:"provider_name,omitempty"`
-	UserId                       int64       `json:"user_id,omitempty"`
-	GameId                       string      `json:"game_id,omitempty"`
-	GameName                     string      `json:"game_name,omitempty"`
-	GameAction                   string      `json:"game_action,omitempty"` // "rollback"
-	RoundId                      int64       `json:"round_id,omitempty"`
-	BetId                        int64       `json:"bet_id,omitempty"`
-	TransactionId                int64       `json:"transaction_id,omitempty"`
-	FeeGroup                     string      `json:"fee_group,omitempty"`
-	Currency                     string      `json:"currency,omitempty"`
-	SettlementCurrency           string      `json:"settlement_currency,omitempty"`
-	ExchangeRate                 string      `json:"exchange_rate,omitempty"`
-	AmountCurrency               string      `json:"amount_currency,omitempty"`
-	AmountSettlementCurrency     string      `json:"amount_settlement_currency,omitempty"`
-	AmountUsd                    string      `json:"amount_usd,omitempty"`
-	AmountReportingCurrency      string      `json:"amount_reporting_currency,omitempty"`
-	CashAmountCurrency           string      `json:"cash_amount_currency,omitempty"`
-	CashAmountSettlementCurrency string      `json:"cash_amount_settlement_currency,omitempty"`
-	CashAmountUsd                string      `json:"cash_amount_usd,omitempty"`
-	CashAmountReportingCurrency  string      `json:"cash_amount_reporting_currency,omitempty"`
-	Turnover                     string      `json:"turnover,omitempty"`
-	TurnoverUsd                  string      `json:"turnover_usd,omitempty"`
-	TurnoverReportingCurrency    string      `json:"turnover_reporting_currency,omitempty"`
-	CreatedAt                    int64       `json:"created_at,omitempty"`
-	SystemOperatorId             int64       `json:"system_operator_id,omitempty"`
-	RetailerOperatorId           int64       `json:"retailer_operator_id,omitempty"`
-	CompanyOperatorId            int64       `json:"company_operator_id,omitempty"`
-	UserTotalBetCount            int64       `json:"user_total_bet_count,omitempty"`
-	EventInfos                   []EventInfo `json:"event_infos,omitempty"`
-	Type                         string      `json:"type,omitempty"`
-	RollbackType                 string      `json:"rollback_type,omitempty"`
-	UpdateBalance                bool        `json:"update_balance,omitempty"`
-	RollbackOriginalAction       string      `json:"rollback_original_action,omitempty"`
+	OperatorId                   int64            `json:"operator_id,omitempty"`
+	OperatorName                 string           `json:"operator_name,omitempty"`
+	ProviderId                   string           `json:"provider_id,omitempty"`
+	ProviderName                 string           `json:"provider_name,omitempty"`
+	UserId                       int64            `json:"user_id,omitempty"`
+	GameId                       string           `json:"game_id,omitempty"`
+	GameName                     string           `json:"game_name,omitempty"`
+	GameAction                   string           `json:"game_action,omitempty"` // "rollback"
+	RoundId                      int64            `json:"round_id,omitempty"`
+	BetId                        int64            `json:"bet_id,omitempty"`
+	TransactionId                int64            `json:"transaction_id,omitempty"`
+	FeeGroup                     string           `json:"fee_group,omitempty"`
+	Currency                     string           `json:"currency,omitempty"`
+	SettlementCurrency           string           `json:"settlement_currency,omitempty"`
+	ExchangeRate                 string           `json:"exchange_rate,omitempty"`
+	AmountCurrency               string           `json:"amount_currency,omitempty"`
+	AmountSettlementCurrency     string           `json:"amount_settlement_currency,omitempty"`
+	AmountUsd                    string           `json:"amount_usd,omitempty"`
+	AmountReportingCurrency      string           `json:"amount_reporting_currency,omitempty"`
+	CashAmountCurrency           string           `json:"cash_amount_currency,omitempty"`
+	CashAmountSettlementCurrency string           `json:"cash_amount_settlement_currency,omitempty"`
+	CashAmountUsd                string           `json:"cash_amount_usd,omitempty"`
+	CashAmountReportingCurrency  string           `json:"cash_amount_reporting_currency,omitempty"`
+	Turnover                     string           `json:"turnover,omitempty"`
+	TurnoverUsd                  string           `json:"turnover_usd,omitempty"`
+	TurnoverReportingCurrency    string           `json:"turnover_reporting_currency,omitempty"`
+	CreatedAt                    int64            `json:"created_at,omitempty"`
+	SystemOperatorId             int64            `json:"system_operator_id,omitempty"`
+	RetailerOperatorId           int64            `json:"retailer_operator_id,omitempty"`
+	CompanyOperatorId            int64            `json:"company_operator_id,omitempty"`
+	UserTotalBetCount            int64            `json:"user_total_bet_count,omitempty"`
+	EventInfos                   []EventInfo      `json:"event_infos,omitempty"`
+	Type                         string           `json:"type,omitempty"`
+	RollbackType                 string           `json:"rollback_type,omitempty"`
+	UpdateBalance                bool             `json:"update_balance,omitempty"`
+	RollbackOriginalAction       string           `json:"rollback_original_action,omitempty"`
+	AffectedCredits              []AffectedCredit `json:"affected_credits,omitempty"`
 }
 
 type GameBetEvent struct {
