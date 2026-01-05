64c7cec7ae6988a7f2f2c02b4b1ef96a71f5fcbb
davy <davy@speedix.com>
2026-01-05 14:01:32 +0800

    Add operatorWalletTransactionInfo to GetTicketResponse
    - Introduced operatorWalletTransactionInfo field in GetTicketResponse to provide additional wallet transaction details for operators.
    - Updated OpenAPI, protobuf, and validation files to accommodate the new field and ensure proper validation handling.

--- review/service/v1/review.pb.validate.go
+++ review/service/v1/review.pb.validate.go
@@ -2294,6 +2294,35 @@ func (m *GetTicketResponse) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetOperatorWalletTransactionInfo()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GetTicketResponseValidationError{
+					field:  "OperatorWalletTransactionInfo",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GetTicketResponseValidationError{
+					field:  "OperatorWalletTransactionInfo",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetOperatorWalletTransactionInfo()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GetTicketResponseValidationError{
+				field:  "OperatorWalletTransactionInfo",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return GetTicketResponseMultiError(errors)
 	}
